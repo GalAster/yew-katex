e334b356191cd9c575be7e6c207fc704ed0ec2bd
Aster <galaster@foxmail.com>
2020-10-20 11:08:58 +0800

    Deploying to gh-pages from @ e3b1e51 🚀

--- index-d236a8165ad9e9bb.js
+++ index-d236a8165ad9e9bb.js
@@ -209,7 +209,7 @@ function addBorrowedObject(obj) {
 }
 function __wbg_adapter_18(arg0, arg1, arg2) {
     try {
-        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h27015b2369f9e8db(arg0, arg1, addBorrowedObject(arg2));
+        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h3f0ab4620d19ed20(arg0, arg1, addBorrowedObject(arg2));
     } finally {
         heap[stack_pointer++] = undefined;
     }
@@ -493,8 +493,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper281 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 81, __wbg_adapter_18);
+    imports.wbg.__wbindgen_closure_wrapper263 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 82, __wbg_adapter_18);
         return addHeapObject(ret);
     };
 
